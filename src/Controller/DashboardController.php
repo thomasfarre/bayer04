c7f9069d79bd50400dd8c94c5ec75679532daa29
Thomas FARRE <tfarre@sutunam.com>
2023-10-07 10:46:36 +0200

    first draw showing Role and Players on Dashboard template, need to delete unused function and view

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Player;
+use App\Entity\Role;
 use App\Form\PlayerType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,32 +11,57 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user', name: 'user_profile')]
 class DashboardController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user_profile")
-     */
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {}
+
+    public function index(Request $request): Response
     {
-        // Get user information from security token
         $user = $this->getUser();
 
+        $players = $this->findAllPlayers();
+        $roles = $this->findAllRoles();
+
+        [$player, $form] = $this->handlePlayerCreation($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('dashboard', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('dashboard/index.html.twig', [
+            'user' => $user,
+            'player' => $player,
+            'form' => $form,
+            'players' => $players,
+            'roles' => $roles
+        ]);
+    }
+
+    private function findAllPlayers(): array
+    {
+        return $this->entityManager->getRepository(Player::class)->findAll();
+    }
+
+    private function findAllRoles(): array
+    {
+        return $this->entityManager->getRepository(Role::class)->findAll();
+    }
+
+
+    private function handlePlayerCreation(Request $request): array
+    {
         $player = new Player();
         $form = $this->createForm(PlayerType::class, $player);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($player);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('player_index', [], Response::HTTP_SEE_OTHER);
+            $this->entityManager->persist($player);
+            $this->entityManager->flush();
         }
 
-        // Pass user data to the view
-        return $this->render('dashboard/index.html.twig', [
-            'user' => $user,
-            'player' => $player,
-            'form' => $form,
-        ]);
+        return [$player, $form];
     }
 }
--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -13,62 +13,6 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/player')]
 class PlayerController extends AbstractController
 {
-    #[Route('/', name: 'player_index', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): Response
-    {
-        $players = $entityManager->getRepository(Player::class)->findAll();
-
-        return $this->render('player/index.html.twig', [
-            'players' => $players,
-        ]);
-    }
-
-    #[Route('/new', name: 'app_player_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $player = new Player();
-        $form = $this->createForm(PlayerType::class, $player);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($player);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('player_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('player/new.html.twig', [
-            'player' => $player,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_player_show', methods: ['GET'])]
-    public function show(Player $player): Response
-    {
-        return $this->render('player/show.html.twig', [
-            'player' => $player,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_player_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Player $player, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(PlayerType::class, $player);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('player_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('player/edit.html.twig', [
-            'player' => $player,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/{id}', name: 'app_player_delete', methods: ['POST'])]
     public function delete(Request $request, Player $player, EntityManagerInterface $entityManager): Response
     {
@@ -77,6 +21,6 @@ class PlayerController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('player_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('dashboard', [], Response::HTTP_SEE_OTHER);
     }
 }
